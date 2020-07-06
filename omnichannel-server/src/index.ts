a3f01fa05e4a87932ea539c4980e06a797295817
Gus <gus@core-code.io>
2020-07-06 17:36:07 -0600

    Creates index.ts pseudocode and dependency injection Updates bitácora

--- omnichannel-server/src/index.ts
+++ omnichannel-server/src/index.ts
@@ -1,12 +1,23 @@
+import Dependencies from "@corecodeio/libraries/di";
 import { Request, Response } from "express";
+import { MessageSourceControllerInjectionKey } from "./feature/message-source/controller/MessageSourceControllerInjectionKey";
 import server from "./server";
 
-server.post("/", async (req: Request, res: Response) => {
-  console.log(req.body);
-});
+const dependencies = new Dependencies();
 
-server.get("/", async (req: Request, res: Response) => {
-  console.log("GET");
+server.post("/message-source", async (req: Request, res: Response) => {
+  const MessageSourceController = dependencies.provide(
+    MessageSourceControllerInjectionKey
+  );
+
+  // recibir la solicitud (payload de twilio o messagebird MessageSourceProvider)
+  // estandarizar la solicitud (MessageSource{Digest|Parser} || MessageFormatter)
+  // guardar la metadata del mensaje en una base de datos
+  // notificar a maría (native client) que recibió un mensaje
+  // PostRequestServer, MVC, MessageFormatter, MessageController
+  // Dependency Injection
+
+  return res.status(200);
 });
 
 server.listen("8001", () => {
