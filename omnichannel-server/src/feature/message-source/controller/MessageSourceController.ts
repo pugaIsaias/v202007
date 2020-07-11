1bc3b2a0d960f2bf50767c9f0c63b096bd671d3f
Ktoxcon <kennethtoxro@gmail.com>
2020-07-11 17:27:52 -0600

    New message parser related features added

--- omnichannel-server/src/feature/message-source/controller/MessageSourceController.ts
+++ omnichannel-server/src/feature/message-source/controller/MessageSourceController.ts
@@ -1,1 +1,17 @@
-export class MessageSourceController {}
+import { MessageParser } from "../util/MessageParser";
+
+export class MessageSourceController {
+  private messageParser = new MessageParser();
+
+  constructor(messageParser: MessageParser) {
+    this.messageParser = messageParser;
+  }
+
+  initializeMessageParser(rawData: any) {
+    this.messageParser.setRawData(rawData);
+  }
+
+  getMessage() {
+    return this.messageParser.parse();
+  }
+}
