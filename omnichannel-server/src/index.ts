a1bb38cd8558a165f64e8ba342555b55f5935c2b
Ktoxcon <kennethtoxro@gmail.com>
2020-07-11 17:39:09 -0600

    Index updated

--- omnichannel-server/src/index.ts
+++ omnichannel-server/src/index.ts
@@ -1,7 +1,6 @@
 import { Dependencies } from "@corecodeio/libraries/di";
 import { Request, Response } from "express";
-import { MessagePayload } from "./feature/message-source/interfaces/MessagePayload";
-import { MessageSourceControllerInjectionKey } from "./feature/message-source/MessageSourceControllerInjectionKey";
+import { MessageSourceControllerInjectionKey } from "./feature/message-source/injection-keys/MessageSourceControllerInjectionKey";
 import server from "./server";
 
 const dependencies = new Dependencies();
@@ -10,23 +9,7 @@ server.post("/message-source", async (req: Request, res: Response) => {
   const MessageSourceController = dependencies.provide(
     MessageSourceControllerInjectionKey
   );
-
-  const payload: MessagePayload = {
-    message: "",
-    currentTime: new Date(),
-    from: "",
-  };
-
-  // recibir la solicitud (payload de twilio o messagebird MessageSourceProvider)
-
-  // estandarizar la solicitud (MessageSource{Digest|Parser} || MessageFormatter)
-
-  // guardar la metadata del mensaje en una base de datos
-  // notificar a maría (native client) que recibió un mensaje
-  // PostRequestServer, MVC, MessageFormatter, MessageController
-  // Dependency Injection
-
-  return res.status(200);
+  
 });
 
 server.listen("8001", () => {
