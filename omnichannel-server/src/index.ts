37b0b8c5e1b224b28e8898d1d8270efa9fcb5011
Ktoxcon <kennethtoxro@gmail.com>
2020-07-12 19:29:12 -0600

    Simplified index

--- omnichannel-server/src/index.ts
+++ omnichannel-server/src/index.ts
@@ -1,16 +1,7 @@
-import { Dependencies } from "@corecodeio/libraries/di";
-import { Request, Response } from "express";
-import { MessageSourceControllerInjectionKey } from "./feature/message-source/injection-keys/MessageSourceControllerInjectionKey";
 import server from "./server";
+import { sourceController } from "./feature/message-source/controller/MessageController";
 
-const dependencies = new Dependencies();
-
-server.post("/message-source", async (req: Request, res: Response) => {
-  const MessageSourceController = dependencies.provide(
-    MessageSourceControllerInjectionKey
-  );
-  
-});
+server.post("/message-source",sourceController);
 
 server.listen("8001", () => {
   console.log("listening");
