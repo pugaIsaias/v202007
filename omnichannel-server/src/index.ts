7e930850f9db501815fbdb1279845359700beac9
Ktoxcon <kennethtoxro@gmail.com>
2020-07-13 20:57:35 -0600

    Updated index.ts: The respective changes seen in the code session were made

--- omnichannel-server/src/index.ts
+++ omnichannel-server/src/index.ts
@@ -8,27 +8,10 @@ const messageSourceController = dependencies.provide<MessageSourceController>(
   MessageSourceControllerInjectionKey
 );
 
-server.post("/message-source", messageSourceController.messageSource);
-
-// server.post("/message-source", async (req: Request, res: Response) => {
-
-// const payload: MessagePayload = {
-//   message: "",
-//   currentTime: new Date(),
-//   from: "",
-// };
-
-// recibir la solicitud (payload de twilio o messagebird MessageSourceProvider)
-
-// estandarizar la solicitud (MessageSource{Digest|Parser} || MessageFormatter)
-
-// guardar la metadata del mensaje en una base de datos
-// notificar a maría (native client) que recibió un mensaje
-// PostRequestServer, MVC, MessageFormatter, MessageController
-// Dependency Injection
-
-// return res.status(200);
-// });
+server.post(
+  "/message-source",
+  messageSourceController.messageSource.bind(messageSourceController)
+);
 
 server.listen("8001", () => {
   console.log("listening");
