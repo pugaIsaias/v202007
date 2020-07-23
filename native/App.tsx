079f041fbcc5290c7d1d45d17259779391a35f40
Ktoxcon <kennethtoxro@gmail.com>
2020-07-23 15:30:19 -0600

    Updated: directory restructuring

--- native/App.tsx
+++ native/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import OnboardingNavigator from "./src/feature/navigation/navigator/OnboardingNavigator";
+import OnboardingNavigator from "./src/navigator/OnboardingNavigator";
 
 export const App: React.FC<{}> = () => {
   return (
