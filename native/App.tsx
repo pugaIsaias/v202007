18ee57f89caa33d72793c3f3e5b41e8bee1ec58f
Ktoxcon <kennethtoxro@gmail.com>
2020-07-23 13:47:25 -0600

    Updated: NavigationRouter component added

--- native/App.tsx
+++ native/App.tsx
@@ -1,21 +1,8 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import NavigationRouter from "./src/feature/navigation/NavigationRouter";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export const App: React.FC<{}> = () => {
+  return <NavigationRouter />;
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
