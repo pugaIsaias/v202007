faa5ab6671c10fe03d81c973058d5962774285ac
Ktoxcon <kennethtoxro@gmail.com>
2020-07-28 11:34:11 -0600

    Add: Layout directory and ButtonLayout added

---
+++ native/src/common/component/Layout/ButtonLayout.ts
@@ -0,0 +1,5 @@
+import { compose, color, space, display, width, height } from "styled-system";
+
+export const ButtonLayout = compose(color, space, display, width, height);
+
+export default ButtonLayout;
