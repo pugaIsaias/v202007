ee25a82e617ce790c9b0f8d842e4b30ed5c2173c
Ktoxcon <kennethtoxro@gmail.com>
2020-07-28 13:53:20 -0600

    Update: Flexbox support added

--- native/src/common/component/Button/MainActionButton.tsx
+++ native/src/common/component/Button/MainActionButton.tsx
@@ -2,7 +2,18 @@ import React from "react";
 import BaseButton from "./BaseButton";
 
 export const MainActionButton: React.FC<{}> = ({ children, ...props }) => (
-  <BaseButton {...props} children={children} bg="#888" mx={20} my={20} height={50} />
+  <BaseButton
+    {...props}
+    children={children}
+    bg="#888"
+    mx={20}
+    my={20}
+    height={50}
+    borderColor="black"
+    borderRadius={15}
+    alignItems="center"
+    justifyContent="center"
+  />
 );
 
 export default MainActionButton;
