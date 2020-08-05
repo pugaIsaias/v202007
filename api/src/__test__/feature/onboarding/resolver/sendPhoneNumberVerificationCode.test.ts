32044db0e5f54202d9a3b27a6a52d27354b78d64
Gus <gus@core-code.io>
2020-08-05 17:19:49 -0600

    Creates VerifyPhoneNumberCode endpoint

--- api/src/__test__/feature/onboarding/resolver/sendPhoneNumberVerificationCode.test.ts
+++ api/src/__test__/feature/onboarding/resolver/sendPhoneNumberVerificationCode.test.ts
@@ -1,6 +1,10 @@
 import { QuerySendPhoneNumberVerificationCode } from "@corecodeio/libraries/api/onboarding";
+import { Dependencies } from "@corecodeio/libraries/di";
 import request from "supertest";
-import { server } from "../../../../server";
+import { createApolloServer } from "../../../../server";
+
+const dependencies = new Dependencies();
+const server = createApolloServer(dependencies);
 
 describe("sendPhoneNumberVerificationCode", () => {
   test("Should return a response with code 200", async (done) => {
