5dfcd7c47ec7e72f2955fac2b64b905979f7fe74
Gus <gus@core-code.io>
2020-08-10 18:32:13 -0600

    Mocks TwilioSMSVertification dependency

--- api/src/__test__/feature/onboarding/resolver/sendPhoneNumberVerificationCode.test.ts
+++ api/src/__test__/feature/onboarding/resolver/sendPhoneNumberVerificationCode.test.ts
@@ -1,22 +1,35 @@
 import { QuerySendPhoneNumberVerificationCode } from "@corecodeio/libraries/api/onboarding";
 import { Dependencies } from "@corecodeio/libraries/di";
-import request from "supertest";
+import { createTestClient } from "apollo-server-testing";
 import { createApolloServer } from "../../../../server";
+import { TwilioSMSVerificationInjectionKey } from "../../../../util/twilio/InjectionKeys";
+import { ISMSVerification } from "../../../../util/twilio/interface/ISMSVerification";
 
 const dependencies = new Dependencies();
-const server = createApolloServer(dependencies);
+const apolloServer = createApolloServer(dependencies);
+
+dependencies.override<ISMSVerification>(TwilioSMSVerificationInjectionKey, {
+  send: jest.fn(() => Promise.resolve(true)),
+  verify: jest.fn(() => Promise.resolve(true)),
+});
 
 describe("sendPhoneNumberVerificationCode", () => {
-  test("Should return a response with code 200", async (done) => {
-    const res = await request(server)
-      .post(server.graphqlPath)
-      .send({
-        query: QuerySendPhoneNumberVerificationCode,
-        variables: {
-          input: {
-            phoneNumber: "123",
-          },
+  test("Should return a response with code 200", async () => {
+    const { query } = createTestClient(apolloServer);
+
+    const res = await query({
+      query: QuerySendPhoneNumberVerificationCode,
+      variables: {
+        input: {
+          phoneNumber: "123",
         },
-      });
+      },
+    });
+
+    const twilioSMSVerification = dependencies.provide(
+      TwilioSMSVerificationInjectionKey
+    );
+
+    expect(twilioSMSVerification.send).toHaveBeenCalledTimes(1);
   });
 });
