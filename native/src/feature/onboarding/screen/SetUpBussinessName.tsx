05b6b48df4611c288e3fdaebcdd437bb83f9b344
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2020-08-26 12:44:58 -0600

    Create new Screen SetUpBussinessName

---
+++ native/src/feature/onboarding/screen/SetUpBussinessName.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const SetUpBussinessName: React.FC = () => (
+  <View>
+    <Text>CheckCode</Text>
+  </View>
+);
+
+export default SetUpBussinessName;
