d1042c599e41e12a8b793ebd86ec4a978e51fbb5
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2020-08-26 11:51:55 -0600

    Add OnboardingCodeInjectionKey to InjectionKey

--- native/src/feature/onboarding/InjectionKey.ts
+++ native/src/feature/onboarding/InjectionKey.ts
@@ -1,11 +1,21 @@
 import { InjectionKey, InjectionKeyScope } from "@corecodeio/libraries/di";
 import { Onboarding } from "./model/Onboarding";
+import { OnboardingCode } from "./model/OnboardingCode";
 
 export const OnboardingInjectionKey: InjectionKey<Onboarding> = {
   name: "OnboardingInjectionKey",
   scope: InjectionKeyScope.singleton,
   closure: (dependencies) => {
     const onboarding = new Onboarding();
+    return onboarding;
+  },
+};
+
+export const OnboardingCodeInjectionKey: InjectionKey<OnboardingCode> = {
+  name: "OnboardingCodeInjectionKey",
+  scope: InjectionKeyScope.singleton,
+  closure: (dependencies) => {
+    const onboarding = new OnboardingCode();
     return onboarding;
   },
 };
