1261fcf69aa225a5a232c9b299a4ee5affb29501
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2020-09-02 14:24:49 -0600

    add component react-native-confirmation-code-field

---
+++ native/src/common/component/codeInput.tsx
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import { Text } from "react-native";
+import {
+  CodeField,
+  Cursor,
+  useBlurOnFulfill,
+  useClearByFocusCell,
+} from "react-native-confirmation-code-field";
+import styles from "./styles";
+
+const CELL_COUNT = 6;
+
+const codeInput = () => {
+  const [value, setValue] = useState("");
+  const ref = useBlurOnFulfill({ value, cellCount: CELL_COUNT });
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+    value,
+    setValue,
+  });
+
+  const onSetVerificationCode = (code: string) => {
+    console.log(code);
+    setValue(code);
+  };
+
+  return (
+    <CodeField
+      ref={ref}
+      {...props}
+      value={value}
+      onChangeText={onSetVerificationCode}
+      cellCount={CELL_COUNT}
+      rootStyle={styles.codeFiledRoot}
+      keyboardType="number-pad"
+      textContentType="oneTimeCode"
+      renderCell={({ index, symbol, isFocused }) => (
+        <Text
+          key={index}
+          style={[styles.cell, isFocused && styles.focusCell]}
+          onLayout={getCellOnLayoutHandler(index)}
+        >
+          {symbol || (isFocused ? <Cursor /> : null)}
+        </Text>
+      )}
+    />
+  );
+};
