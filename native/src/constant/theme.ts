2eaf98cc176ef3d3e855c5ca536d582e2e1d4243
pugaIsaias <64232550+pugaIsaias@users.noreply.github.com>
2020-09-02 22:36:38 -0600

    created theme

---
+++ native/src/constant/theme.ts
@@ -0,0 +1,7 @@
+export default {
+  colors: {
+    primary: { main: "#28244C", dimmed: "rgba(40,36,76,0.5)" },
+    secondary: { main: "#585884", dimmed: "rgba(88,88,132,0.5)" },
+    black: { main: "#000", dimmed: "rgba(0,0,0,0.5)" },
+  },
+};
