41041a5526ee845d494b03944a1fb600cca006a1
Isaias Puga <64232550+pugaIsaias@users.noreply.github.com>
2020-09-15 16:18:56 -0600

    Update native/src/constant/theme.ts
    space on secondary dimmed
    
    Co-authored-by: Gus <gus@developermexicano.com>

--- native/src/constant/theme.ts
+++ native/src/constant/theme.ts
@@ -3,7 +3,7 @@ export default {
     primary: { main: "rgba(49, 53, 103, 1)", dimmed: "rgba(49, 53, 103, 0.5)" },
     secondary: {
       main: "rgba(68, 69, 116, 1)",
-      dimmed: "rgba(68, 69, 116,0.5)",
+      dimmed: "rgba(68, 69, 116, 0.5)",
     },
     black: { main: "rgba(0, 0, 0, 1)", dimmed: "rgba(0, 0, 0, 0.5)" },
   },
